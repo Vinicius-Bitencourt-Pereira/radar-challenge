6bf290dd5f0f3aacacd58124adead8ce8825c6fb
Vinicius-Bitencourt-Pereira <viniciusbitencourtpereira@gmail.com>
2022-12-08 16:03:09 -0300

    Project created

---
+++ backend/src/main/java/br/com/radar/challenge/RadarProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.radar.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadarProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadarProjectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/radar/challenge/RadarProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.radar.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadarProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
